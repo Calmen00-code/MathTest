ae62d13cc6373f0a67ec012bd932d4bf8d1b40ac
calmen <calmenchia@gmail.com>
2021-10-16 19:55:10 +0800

    Add: fragment for manual input

--- app/src/main/java/com/calmen/mathtest/online_service/LoadTestQuestion.java
+++ app/src/main/java/com/calmen/mathtest/online_service/LoadTestQuestion.java
@@ -56,10 +56,20 @@ public class LoadTestQuestion extends AppCompatActivity {
         countdown = findViewById(R.id.timeCountdownView);
         question = findViewById(R.id.questionView);
 
+        /*
         if (showQuestion) {
             new DownloaderTask().execute();
             showQuestion = !showQuestion;
         }
+         */
+
+        new DownloaderTask().execute();
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DownloaderTask().execute();
+            }
+        });
     }
 
     private class DownloaderTask extends AsyncTask<Void, Integer, String> {
@@ -134,14 +144,14 @@ public class LoadTestQuestion extends AppCompatActivity {
             question.setText(items[0]);
 
             if (items[2].equals("")) {
-                submit.setVisibility(View.INVISIBLE);
+                submit.setVisibility(View.VISIBLE);
+
+                // User input answer using manual input if there is no option(s)
+                activateManualInput();
             } else {
+                submit.setVisibility(View.INVISIBLE);
                 String[] optionsArr = items[2].split(",");
 
-                if (optionsArr.length == 0) {
-                    // User input answer using manual input if there is no option(s)
-                    activateManualInput();
-                }
             }
         }
 
@@ -191,7 +201,6 @@ public class LoadTestQuestion extends AppCompatActivity {
          * Change the answer mode as user manual input mode
          */
         public void activateManualInput() {
-            submit.setVisibility(View.VISIBLE);
 
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction transaction = fm.beginTransaction();
