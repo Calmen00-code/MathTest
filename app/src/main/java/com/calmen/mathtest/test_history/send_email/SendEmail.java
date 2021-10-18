8daaeb66af219f51b1d42ded6092186608e1b0f2
calmen <calmenchia@gmail.com>
2021-10-18 11:38:27 +0800

    Add: Sending result in email

--- app/src/main/java/com/calmen/mathtest/test_history/send_email/SendEmail.java
+++ app/src/main/java/com/calmen/mathtest/test_history/send_email/SendEmail.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import com.calmen.mathtest.R;
 import com.calmen.mathtest.models.Email;
 import com.calmen.mathtest.models.EmailList;
+import com.calmen.mathtest.models.Student;
+import com.calmen.mathtest.models.StudentList;
 import com.calmen.mathtest.test_history.recycler_test_history.RecyclerTestAdapter;
 import com.calmen.mathtest.test_history.send_email.recycler_view_email.RecyclerEmailAdapter;
 
@@ -16,22 +18,30 @@ import java.util.ArrayList;
 
 public class SendEmail extends AppCompatActivity {
     ArrayList<Email> emails;
+    ArrayList<Student> students;
+    public static final String EMAIL_TITLE = "Test Result";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_email);
 
+        loadStudents();
         loadEmails();
 
         RecyclerView rv = findViewById(R.id.listViewEmailRecycler);
         rv.setLayoutManager(new LinearLayoutManager(this));
-        RecyclerEmailAdapter recyclerEmailAdapter = new RecyclerEmailAdapter(emails, this);
+        RecyclerEmailAdapter recyclerEmailAdapter = new RecyclerEmailAdapter(emails, students, this);
         rv.setAdapter(recyclerEmailAdapter);
     }
 
     public void loadEmails() {
         EmailList emailList = new EmailList();
         emails = emailList.getEmails(this);
+    }
+
+    public void loadStudents() {
+        StudentList studentList = new StudentList();
+        students = studentList.getStudents(this);
     }
 }
--- app/src/main/java/com/calmen/mathtest/test_history/send_email/recycler_view_email/RecyclerEmailAdapter.java
+++ app/src/main/java/com/calmen/mathtest/test_history/send_email/recycler_view_email/RecyclerEmailAdapter.java
@@ -1,6 +1,9 @@
 package com.calmen.mathtest.test_history.send_email.recycler_view_email;
 
+import static com.calmen.mathtest.test_history.send_email.SendEmail.EMAIL_TITLE;
+
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +13,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.calmen.mathtest.R;
 import com.calmen.mathtest.models.Email;
+import com.calmen.mathtest.models.Student;
 import com.calmen.mathtest.test_history.recycler_test_history.RecyclerTestViewHolder;
 
 import java.util.ArrayList;
 
 public class RecyclerEmailAdapter extends RecyclerView.Adapter<RecyclerEmailViewHolder> {
     ArrayList<Email> emails;
+    ArrayList<Student> students;
     Context context;
 
-    public RecyclerEmailAdapter(ArrayList<Email> inEmails, Context inContext) {
+    public RecyclerEmailAdapter(ArrayList<Email> inEmails, ArrayList<Student> inStudents, Context inContext) {
         this.emails = inEmails;
+        this.students = inStudents;
         this.context = inContext;
     }
 
@@ -40,7 +46,16 @@ public class RecyclerEmailAdapter extends RecyclerView.Adapter<RecyclerEmailView
         holder.selectEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                String [] mailto = {email.getEmail()};
+                String subject = EMAIL_TITLE;
+                String mailbody = getResult();
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("message/rfc822");
+                intent.putExtra(Intent.EXTRA_EMAIL, mailto);
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                intent.putExtra(Intent.EXTRA_TEXT, mailbody);
+                view.getContext().startActivity(intent);
             }
         });
     }
@@ -48,5 +63,15 @@ public class RecyclerEmailAdapter extends RecyclerView.Adapter<RecyclerEmailView
     @Override
     public int getItemCount() {
         return emails.size();
+    }
+
+    public String getResult() {
+        String result = "";
+
+        for (Student student: students) {
+            result += student.getFirstname() + " " + student.getLastname() + ": " + student.getScore();
+            result += "\n";
+        }
+        return result;
     }
 }
